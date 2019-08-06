5bfd201786b215fa13c7280d8b50d630cdf52ee1
ygladiensyah <ygladiensyah@gmail.com>
2019-08-06 10:31:30 +0700

    add landing page

--- app/src/main/java/com/nusantarian/autoscad/Activity/LandingActivity.java
+++ app/src/main/java/com/nusantarian/autoscad/Activity/LandingActivity.java
@@ -2,15 +2,46 @@ package com.nusantarian.autoscad.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.nusantarian.autoscad.R;
 
-public class LandingActivity extends AppCompatActivity {
+public class LandingActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.btn_login).setOnClickListener(this);
+        findViewById(R.id.btn_register).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        if (mUser != null){
+            startActivity(new Intent(LandingActivity.this, MainActivity.class));
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_login:
+                startActivity(new Intent(LandingActivity.this, LoginActivity.class));
+                break;
+            case R.id.btn_register:
+                startActivity(new Intent(LandingActivity.this, RegisterActivity.class));
+                break;
+        }
     }
 }
