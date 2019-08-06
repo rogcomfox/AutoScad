858f3b7cb4df4fdecb795149439910e2e19ec73d
ygladiensyah <ygladiensyah@gmail.com>
2019-08-06 11:28:17 +0700

    add authentication page

--- app/src/main/java/com/nusantarian/autoscad/Activity/LoginActivity.java
+++ app/src/main/java/com/nusantarian/autoscad/Activity/LoginActivity.java
@@ -3,14 +3,37 @@ package com.nusantarian.autoscad.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.nusantarian.autoscad.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth;
+    private EditText et_email, et_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        et_email = findViewById(R.id.et_email);
+        et_password = findViewById(R.id.et_password);
+        findViewById(R.id.btn_login).setOnClickListener(this);
+        findViewById(R.id.tv_lupa).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_login:
+                String email = et_email.getText().toString();
+                String password = et_password.getText().toString();
+                break;
+            case R.id.tv_lupa:
+        }
     }
 }
---
+++ app/src/main/java/com/nusantarian/autoscad/Activity/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.nusantarian.autoscad.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nusantarian.autoscad.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
