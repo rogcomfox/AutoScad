6066379a787c4adc92a40d2002dbf6e1002e0239
ygladiensyah <ygladiensyah@gmail.com>
2019-09-01 04:48:26 +0700

    add about page

--- app/src/main/java/com/nusantarian/autoscad/Activity/AboutActivity.java
+++ app/src/main/java/com/nusantarian/autoscad/Activity/AboutActivity.java
@@ -1,16 +1,26 @@
 package com.nusantarian.autoscad.Activity;
 
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import com.nusantarian.autoscad.R;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        findViewById(R.id.img_back).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.img_back){
+            onBackPressed();
+        }
     }
 }
