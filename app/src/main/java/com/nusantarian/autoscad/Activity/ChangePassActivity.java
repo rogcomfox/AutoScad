0237ebc256e3977ea83a1d82753effea2e118fe1
ygladiensyah <ygladiensyah@gmail.com>
2019-09-01 13:01:14 +0700

    add change password page

---
+++ app/src/main/java/com/nusantarian/autoscad/Activity/ChangePassActivity.java
@@ -0,0 +1,14 @@
+package com.nusantarian.autoscad.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.nusantarian.autoscad.R;
+
+public class ChangePassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_pass);
+    }
+}
